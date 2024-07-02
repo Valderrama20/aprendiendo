0215a83ee1ada05bef58d596f379b2d3bd405856
Valderrama20 <jose23122009@gmai.com>
2024-07-02 13:50:51 +0200

    aprendiendo git

--- Git/git.ts
+++ Git/git.ts
@@ -31,4 +31,22 @@
 // git add . : me agrega todos los archivos
 // git add *.js : me agrega solo los archivos con la extension indicada
 // git add prueba.js : me agrega solo el archivo que indique, se pueden agregar varios archivos
-// git restore <archivo> : revierte los cambios de la carpeta al ultimo commit
+// git restore <archivo> : revierte los cambios hicimos a dicho archivo
+// git restore --staged <archivo> : elimina este cambio del staged, es como
+// - si hicieramos un git add <archivo> y luego revertimos ese git add
+// git rm <nombre de archivo> : elimina una archivo y lo agrega a staged
+// git mv <nombreDeArchivo> <nuevoNombre> : me cambia el nombre de un archivo
+// git status -s : me muestra en que estado estan mis archivios per sin informacion redundante
+// git log --oneline : me muetra todo el historial de commits
+
+// trabajo en equipo
+
+// git branch : me muestra todas la ramas existentes
+// git checkout -b <nombreDeRama> : me crea y cambia a la rama creada directamente
+// git checkout <nombreDeRama> : me cambia a la rama indicada
+// cat <nombreDeArchivo> : me muestra el contenido de mi archivo
+// git merge <ramaParaCombinar> : estando parado en mi rama principal puedo usar este
+// - comando para combinar ramas
+// git remote add origin <url dle repo> : esto me indica de donde voy a obtener los cambios
+// - en mi repo y a donde voy a subir cambios
+// git push -u origin <rama> : sube los cambios y crea la rama en mi repo en gitHub
