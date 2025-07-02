b9b102e719e1cadc2b8f5988b1167f7353e21b0d
valderrama20 <jose23122009@gmail.com>
2025-07-02 11:21:34 +0200

    Expresiones en rust

---
+++ rust/guia_practica_rust/07_expresiones/01_expresiones.rs
@@ -0,0 +1,28 @@
+// -------- Expresiones ----------
+
+// Un programa de Rust se compone (en su mayoria) de una serie de declaraciones
+
+fn main() {
+    // statement
+    // statement
+    // statement
+}
+
+// Hay algunos tipos de declaraciones en Rust. Las dos mas comunes son declarar
+// una variable y utilizar ";" al final
+
+fn main() {
+    // variable binding
+    let x = 5;
+
+    // expression;
+    x;
+    x + 1;
+    15;
+}
+
+// Los bloques tambien son expresiones, por lo cual de pueden utilizar como
+// valores en una asignacion
+// la ultima exppresion del bloque simpre y cuando no lleve ";"
+// sera el valor que este tome
+
