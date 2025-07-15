e99c1feca1d2cf2c9e2fe454dc7bf21aa5a4d50e
Jose Garcia <88403105+Valderrama20@users.noreply.github.com>
2025-07-15 08:56:39 +0200

    Delete rust/video_tutorial_4horas/episodio07/teoria/target directory

--- rust/video_tutorial_4horas/episodio07/teoria/target/debug/teoria.d
+++
@@ -1,1 +0,0 @@
-/home/valderrama20/Escritorio/aprendiendo/rust/video_tutorial_4horas/episodio07/teoria/target/debug/teoria: /home/valderrama20/Escritorio/aprendiendo/rust/video_tutorial_4horas/episodio07/teoria/src/main.rs
