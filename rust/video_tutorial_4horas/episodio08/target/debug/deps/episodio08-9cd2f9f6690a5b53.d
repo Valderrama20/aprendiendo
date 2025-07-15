6c83d72ededa1b8291bf8fd40779356d907dd5d7
Jose Garcia <88403105+Valderrama20@users.noreply.github.com>
2025-07-15 08:57:14 +0200

    Delete rust/video_tutorial_4horas/episodio08/target directory

--- rust/video_tutorial_4horas/episodio08/target/debug/deps/episodio08-9cd2f9f6690a5b53.d
+++
@@ -1,5 +0,0 @@
-/home/valderrama20/Escritorio/aprendiendo/rust/video_tutorial_4horas/episodio08/target/debug/deps/episodio08-9cd2f9f6690a5b53: src/main.rs
-
-/home/valderrama20/Escritorio/aprendiendo/rust/video_tutorial_4horas/episodio08/target/debug/deps/episodio08-9cd2f9f6690a5b53.d: src/main.rs
-
-src/main.rs:
--- rust/video_tutorial_4horas/episodio08/target/debug/episodio08.d
+++
@@ -1,1 +0,0 @@
-/home/valderrama20/Escritorio/aprendiendo/rust/video_tutorial_4horas/episodio08/target/debug/episodio08: /home/valderrama20/Escritorio/aprendiendo/rust/video_tutorial_4horas/episodio08/src/main.rs
