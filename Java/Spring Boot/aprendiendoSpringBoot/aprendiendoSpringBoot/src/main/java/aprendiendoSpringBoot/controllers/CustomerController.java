522e6f11c77706236a56261bceb0b0d4c9fb9cea
valderrama20 <jose23122009@gmail.com>
2026-01-04 14:03:39 +0100

    JAVA: agregando uri en la respuesta de creacion de un recurso

--- Java/Spring Boot/aprendiendoSpringBoot/aprendiendoSpringBoot/src/main/java/aprendiendoSpringBoot/controllers/CustomerController.java
+++ Java/Spring Boot/aprendiendoSpringBoot/aprendiendoSpringBoot/src/main/java/aprendiendoSpringBoot/controllers/CustomerController.java
@@ -4,7 +4,9 @@ import aprendiendoSpringBoot.domain.Customer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -42,7 +44,16 @@ public class CustomerController {
     public ResponseEntity<?> postCreateClient(@RequestBody Customer customer) {
         customers.add(customer);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body("Cliente creado correctamente: " + customer.getUsername());
+        // Creaos la uri del recurso creado
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{username}")
+                .buildAndExpand(customer.getUsername())
+                .toUri();
+
+        // return ResponseEntity.created(location).build();
+        return ResponseEntity.created(location).body(customer);
+
     }
 
     // @RequestMapping(method = RequestMethod.PUT)
